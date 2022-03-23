8b744e26e7f21a048325156af44c7fd989bc7af1
mbruks <68144505+mbruks@users.noreply.github.com>
2022-03-23 19:56:30 +0300

    Задание 1.2

--- Laba_2/laba_2.rb
+++ Laba_2/laba_2.rb
@@ -1,10 +1,18 @@
 class Department
   attr_accessor :name, :phone
 
-  def initialize(name, phone)
+  def initialize(name, phone, post)
     @name = name
     @phone = phone
+    @post = post
   end
 
+  def to_s
+    "#{@name}, #{@phone}, #{@post}"
+  end
+  
 end
 
+sotr1 = Department.new("Maria", "8-918-261-02-68")
+sotr2 = Department.new("Astra", "8-918-433-85-78")
+puts sotr1, sotr2
