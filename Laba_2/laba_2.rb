102655b6529afab58c932b2a75fb6251b3a16017
mbruks <68144505+mbruks@users.noreply.github.com>
2022-03-24 17:28:26 +0300

    Задание 2.2

--- Laba_2/laba_2.rb
+++ Laba_2/laba_2.rb
@@ -57,6 +57,28 @@ class Department
   
 end
 
+
+def read_from_txt(file)
+  reg_name = /^"[\w| |,|.]+/
+  reg_phone = /8\-?([0-9]{3})\-?([ .-]?)([0-9]{3})\2([0-9]{4})/
+  reg_post = /\(["[\w| |,|.|']+"]*\)$/
+  post = []
+  File.open(file,"r") do |f|
+    while (line = f.gets)
+      name = reg_name.match(line).to_s
+      name[0]=''
+      phone = reg_phone.match(line).to_s
+      duties = reg_post.match(line).to_s
+      pp = Department.find_duties(duties)
+      pos = Department.new(name, phone)
+      (0..pp.length-1).each { |x| pos.add_post(pp[x]) }
+      post.append(pos)
+    end
+  end
+  post
+end
+
+
 def ask_post(pos)
   puts "Какую обязанность выбрать?"
   puts pos.print_post
