6386e7901d60dc037734488196e73a75cddb6751
mbruks <68144505+mbruks@users.noreply.github.com>
2022-05-23 17:16:13 +0300

    Update Department_list.rb
    задание 3.2

--- Laba_2/Department_list.rb
+++ Laba_2/Department_list.rb
@@ -9,4 +9,36 @@ class Department_list
     list.each { |i| add_note(i) }
     @index = 0
   end
+  
+    # добавить запись
+  def add_note(dep) 
+    @dep_list.append(dep)
+  end
+
+  # выделить запись
+  def choose_note(index)
+    @index = index
+  end
+
+  # заменить запись
+  def change_note(new_dep)
+    @dep_list[@index] = new_dep
+  end
+
+  # возвращающий выбранную запись
+  def get_note
+    @dep_list[@index]
+  end
+
+  # удаляющий выбранную запись
+  def delete_note
+    @dep_list.delete_at(@index)
+    @index-=1
+  end
+
+  def to_s
+    s = "\n"
+    @dep_list.each_index { |i| s += "#{i}}  "+@dep_list[i].to_s }
+    s
+  end
  end
