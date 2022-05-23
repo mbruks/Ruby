fa287f7c5f6245dc008dacfb4014f998a9e73fca
mbruks <68144505+mbruks@users.noreply.github.com>
2022-05-23 17:36:45 +0300

    Update Department_list.rb
    задание 3.3

--- Laba_2/Department_list.rb
+++ Laba_2/Department_list.rb
@@ -41,4 +41,68 @@ class Department_list
     @dep_list.each_index { |i| s += "#{i}}  "+@dep_list[i].to_s }
     s
   end
+  
+  def Department_list.from_txt(file)
+    reg_name = /^"[\w| |,|.]+/
+    reg_phone = /8\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/
+    reg_duties = /\(["[\w| |,|.]+"]*\)$/
+    deps = Department_list.new
+    File.open(file,"r") do |f|
+      while (line = f.gets)
+        name = reg_name.match(line).to_s
+        name[0]=''
+        phone = reg_phone.match(line).to_s
+        duties = reg_duties.match(line).to_s
+        d = find_duties(duties)
+        dep = Department.new(name, phone)
+        (0..d.length-1).each { |x| dep.set_duty(d[x]) }
+        deps.add_note(dep)
+      end
+    end
+    deps
+  end
+
+  def each
+    @dep_list.each  do |dep|
+     yield dep
+    end
+  end
+
+  def Department_list.to_txt(file, deps)
+    File.open(file,"w") do |f|
+      deps.each do |x|
+        f.puts "\"#{x.name}\", #{x.phone}, (#{x.duties_for_txt})"
+      end
+    end
+  end
+
+  def Department_list.from_yaml(file)
+    store = YAML::Store.new file
+    arr_lines = ""
+    File.open(file,"r") do |f|
+      while (line = f.gets)
+        arr_lines+= line
+      end
+    end
+    deps = store.load(arr_lines)
+    d = Department_list.new
+    deps.each { |i| d.add_note(i) }
+    d
+  end
+
+  def Department_list.to_yaml(file, deps)
+    File.open(file,"w") do |f|
+      f.puts YAML.dump(deps)
+    end
+  end
  end
+
+
+=begin
+b = Department_list.from_yaml "yaml_text.yaml"
+puts b
+Department_list.to_txt("Department.txt",b)
+a = Department_list.from_txt("Department.txt")
+puts a
+Department_list.to_yaml("yaml_text.yaml",a)
+end
